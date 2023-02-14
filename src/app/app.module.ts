fe5e0829e3c7ccd70a3384ff221d8343a473dd34
Joel Becker <joelbecker2219@gmail.com>
2023-02-14 17:17:45 -0300

    version one-ComicApp

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+//rutas
+import {APP_ROUTING} from '../app/app.routes';
+
+//servicios
+import{HeroesService} from './sevicios/heroes.service';
+
+
+//componentes
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroeComponent } from './components/heroe/heroe.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent,
+    HeroeComponent,
+    BuscadorComponent,
+    
+    
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    APP_ROUTING,
+  ],
+  providers: [
+    HeroesService
+    
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
